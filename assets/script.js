2c235ea89d854be09e41c9f922a4a5b0210171d3
Shmeeheart <Shmeeheart.goodwije@gmail.com>
2022-04-24 19:18:40 -0600

    updated search list details

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
     <script src="https://apis.google.com/js/api.js"></script>
 <script>
@@ -8,16 +16,27 @@
    */
    var youTubeurl= "https://www.youtube.com/watch?v=" 
 
+   function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+    }
+
   function loadClient() {
     gapi.client.setApiKey("AIzaSyDoUXzepXuwRqDCS65-J-rld4zpvZlOO1U");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
-  }
+    }
   // Make sure the client is loaded and sign-in is complete before calling this method.
   function execute() {
     return gapi.client.youtube.search.list({
-        q:"Sound Bath"
+        "part": [
+            "snippet"
+        ],
+        "maxResults": 25,
+        "q": "Sound Bath"
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
@@ -33,3 +52,4 @@
 <button onclick="loadClient()">authorize and load</button>
 <button onclick="execute()">execute</button>    
 </body>
+</html>
