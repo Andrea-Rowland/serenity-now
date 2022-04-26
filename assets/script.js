93b14633ceba34c0f47b3dde98d4300b74e37fb7
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-04-26 13:22:25 -0600

    copied Jen's js and worked on Local Storage

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+    localStorage.setItem(".name", ".birth");
+
+
+    // window.addEventListener("load", function(event){
+    //     let myPromise = new Promise(function(myResolve, myReject) {
+    //         // "Producing Code" (May take some time)
+            
+    //           myResolve(); // when successful
+    //           myReject();  // when error
+    //         });
+            
+    //         // "Consuming Code" (Must wait for a fulfilled Promise)
+    //         myPromise.then(
+    //           function(value) { /* code if successful */ },
+    //           function(error) { /* code if some error */ }
+    //         );
+    //     gapi.load("client:auth2", function() { 
+    //         console.log("Sound Bath")
+    //         gapi.auth2.init({client_id: "63303441351-oj03veb03olj4le9bcvas5n873tiaq4k.apps.googleusercontent.com"});
+    //       }); 
+    //     loadClient()
+    // })
+    var youTubeurl= "https://www.youtube.com/watch?v=" 
+    setTimeout(()=>{
+     loadClient()
+    },2000)
+     
+ //    function authenticate() {
+ //    return gapi.auth2.getAuthInstance()
+ //       .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+ //       .then(function() { console.log("Sign-in successful"); },
+ //               function(err) { console.error("Error signing in", err); });
+ //     }
+ 
+   function loadClient() {
+     gapi.client.setApiKey("AIzaSyDoUXzepXuwRqDCS65-J-rld4zpvZlOO1U");
+     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+         .then(function() { console.log("GAPI client loaded for API");execute() },
+               function(err) { console.error("Error loading GAPI client for API", err); });
+     }
+   // Make sure the client is loaded and sign-in is complete before calling this method.
+   function execute() {
+     return gapi.client.youtube.search.list({
+         "part": [
+             "snippet"
+         ],
+         "maxResults": 25,
+         "q": "Sound Bath"
+     })
+         .then(function(response) {
+                 // Handle the results here (response.result has the parsed body).
+                 console.log("Response", response);
+               },
+               function(err) { console.error("Execute error", err); });
+   }
+   
