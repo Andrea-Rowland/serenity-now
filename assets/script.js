1d394c9fad72d07953b42c58e15c627a0d7f5f75
Shmeeheart <Shmeeheart.goodwije@gmail.com>
2022-05-01 15:33:02 -0600

    changing console log to sound bath

--- assets/script.js
+++ assets/script.js
@@ -62,6 +62,25 @@
               function(err) { console.error("Execute error", err); });
   }
 
+  fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+
+  const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://type.fit/api/quotes",
+    "method": "GET"
+  }
+  
+  $.ajax(settings).done(function (response) {
+    const data = JSON.parse(response);
+    console.log(data);
+  });
 
   
  
